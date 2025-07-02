f1a89db000c4f9b18ec820011930f50c5414607f
Meenatechjays <meena.sivakumar@techjays.com>
2025-07-02 10:47:47 +0530

    Widget Implemented

--- App.tsx
+++ App.tsx
@@ -4,20 +4,7 @@ import { PaperProvider } from 'react-native-paper';
 import AppNavigator, { navigationRef } from './src/navigation/AppNavigator';
 
 function App(): React.JSX.Element {
-  useEffect(() => {
-    const checkInitialIntent = async () => {
-      if (Platform.OS === 'android') {
-        const initialIntent = await NativeModules.IntentLauncher?.getInitialIntent?.();
-        const screen = initialIntent?.screen;
-
-        if (screen === 'food') navigationRef?.navigate('FoodLogs');
-        else if (screen === 'movement') navigationRef?.navigate('Movements');
-        else if (screen === 'activity') navigationRef?.navigate('Activity');
-      }
-    };
-
-    checkInitialIntent();
-  }, []);
+  
   return (
     <PaperProvider>
       <AppNavigator />
